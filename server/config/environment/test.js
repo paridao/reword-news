da14765c24c06e8e049202dab2a2c63ae6a0f059
Paride Giovacchini <p.giovacchini@cineca.it>
2018-07-05 10:15:26 +0200

    initial commit

---
+++ server/config/environment/test.js
@@ -0,0 +1,22 @@
+/*eslint no-process-env:0*/
+
+// Test specific configuration
+// ===========================
+module.exports = {
+    // MongoDB connection options
+    mongo: {
+        useMongoClient: true,
+        uri: 'mongodb://localhost/rewordnews-test'
+    },
+    sequelize: {
+        uri: 'sqlite://',
+        options: {
+            logging: false,
+            storage: 'test.sqlite',
+            define: {
+                timestamps: false
+            }
+        }
+    },
+    port: '9001',
+};
